cfc546b0deb05494ad37ee016b6320b70b7e35ec
JerryLeeFighting <drlgc2014@163.com>
2014-06-24 09:38:13 +0800

    让图片滚动起来吧

---
+++ 滚动的图片/滚动的图片/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  滚动的图片
+//
+//  Created by 萨斯辈的呼唤 on 14-6-18.
+//  Copyright (c) 2014年 萨斯辈的呼唤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ 滚动的图片/滚动的图片/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  滚动的图片
+//
+//  Created by 萨斯辈的呼唤 on 14-6-18.
+//  Copyright (c) 2014年 萨斯辈的呼唤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
